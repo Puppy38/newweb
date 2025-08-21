32fb8cbc984cbee832d3022c15d0dd9f70040ec1
Puppy38 <puppyspark38@gmail.com>
2025-08-21 14:39:21 +0300

    Update script.js

--- support/script.js
+++ support/script.js
@@ -4,7 +4,7 @@ function message() {
 
   const webhook =
 "https://discord.com/api/webhooks/1405538730932175020/tPL8K4Mo7rZi-z1v2JjLXONY0KomVqSpL6fX4pgTUr4cu98xcAOM83p-L8ltukw1wBqS";
-  const contents = 'Name: $[name]\nMessage: $[message]';
+  const contents = `Name: ${name}\nMessage: ${message}`;
   const request = new XMLHttpRequest();
   request.open("POST", webhook);
   request.setRequestHeader('Content-type', 'application/json');
@@ -14,6 +14,7 @@ function message() {
   request.send(JSON.stringify(params));
 
 }
+
 
 
 
